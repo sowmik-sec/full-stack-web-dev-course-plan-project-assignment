0562f069cd2461603088cedcbdd5b41a4a64bf8a
sowmik-sec <sowmik.sec@gmail.com>
2023-02-15 17:52:52 +0600

    fake database created and added basic css rules

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="courses">
+        <h1>Here are the courses</h1>
+      </div>
+      <div className="course-detail">
+        <h1>Here are the details</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
